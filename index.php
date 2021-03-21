408c0fefaa086e613ca5d585591c6ad87b1cb9d5
alipiaibrahim <ibrahimalipia56@gmail.com>
2021-03-21 20:33:01 +0700

    menambahkan about

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
     integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 <body>
-    
+<!-- ABOUT -->
+<section id="about">
+    <div class="container">
+        <div class="row about mb-5 pt-5">
+            <div class="col">
+                <h2>ABOUT</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="../img/roti.png" alt="">
+            </div>
+            <div class="col-lg-5">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste odit iusto neque expedita laboriosam, 
+                debitis dignissimos labore vel tempore eos voluptates fugit commodi et delectus accusantium quasi nesciunt. Maiores, non?</p>
+            </div>
+        </div>
+    </div>
+</section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
