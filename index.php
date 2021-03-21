02a492f20c70a76801e0bc50faecde50886ca672
fadilahnurlaela <fadilahnurlaila366@gmail.com>
2021-03-21 10:02:25 -0700

    menambah menu minuman tradisional

--- index.php
+++ index.php
@@ -1,16 +1,242 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="style-anggota2.css">
 </head>
+
 <body>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  <!-- menu minuman  -->
+  <section class="food" id="food" style="padding-top:100px;  background-image: url('img/background.jpeg');  padding-top:100px; margin-top:70px; background-size:cover; min-height:100vh" ;>
+
+
+    <div class="container-custome shop-page">
+      <div class="cof-header">
+        <div class="box-title">
+          <div class="box-title-content">
+            <p style="font-size: 30px; font-weight: bold; text-align:center; " class="section-title">Drink From Indonesia </p>
+
+          </div>
+        </div>
+
+
+
+        <section class="menu-makanan">
+          <table>
+            <tr>
+              <td>
+                <div class="list-product">
+                  <div class="container-fluid">
+                    <div class="row" style="padding-bottom:-1000px;">
+                      <div class="col">
+                        <div class="card">
+                          <img src="./img/minum1 bir pletok betawi.jpg" alt="Avatar" class="img-fluid">
+                          <div class="overlay">
+                            <div class="text">
+                              <p style="font-size:15px; text-align:center;">Bir Pletok Betawi</p>
+                              <hr>
+                              <p style="font-size:10px; text-align:center;">Bir pletok terkenal sebagai minuman khas dari Betawi.Bir pletok memiliki rasa pedas rempah, harum, dan dapat menghangatkan tubuh.</p>
+                            </div>
+                            <div class="card">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <div class="list-product">
+                  <div class="container-fluid">
+                    <div class="row" style="padding-bottom:-1000px;">
+                      <div class="col">
+                        <div class="card">
+                          <img src="./img/minum 2 dawet ayu banjarnegara.jpg" alt="Avatar" class="img-fluid">
+                          <div class="overlay">
+                            <div class="text">
+                              <p style="font-size:15px; text-align:center">Dawet Ayu Banjarnegara</p>
+                              <hr>
+                              <p style="font-size:10px; text-align:center;"> Es Dawet ayu asli khas Banjarnegara lezat serta segar dan sangat cocok diminum pada cuaca panas,
+                                es dawet dapat diminum panas atau pun dingin dengan menambahkan es batu. Rasanya yang segar.</p>
+                            </div>
+                            <div class="card">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+
+              <td>
+                <div class="list-product">
+                  <div class="container-fluid">
+                    <div class="row" style="padding-bottom:-1000px;">
+                      <div class="col">
+                        <div class="card">
+                          <img src="./img/minum3 jamu jun semnarang.jpg" alt=" Avatar" class="img-fluid">
+                          <div class="overlay">
+                            <div class="text">
+                              <p style="font-size:15px; text-align:center">Jamu Jun Semarang</p>
+                              <hr>
+                              <p style="font-size:10px; text-align:center;">Jamu Jun merupakan minuman berbahan dasar tepung beras yang diberi campuran rempah rempah dan di sajikan dengan bubuk merica sebagai pelengkap.
+                                Minuman ini berasal dari dengan semarang</p>
+                            </div>
+                            <div class="card">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+
+              <td>
+                <div class="list-product">
+                  <div class="container-fluid">
+                    <div class="row" style="padding-bottom:-1000px;">
+                      <div class="col">
+                        <div class="card">
+                          <img src="./img/minum4 wedang kacang magelang.jpg" alt="Avatar" class="img-fluid">
+                          <div class="overlay">
+                            <div class="text">
+                              <p style="font-size:15px; text-align:center">Wedang Kacang Magelang</p>
+                              <hr>
+                              <p style="font-size:10px; text-align:center;">Wedang kacang ini berasal dari magelang yang disajikan dalam mangkuk bersama dengan ketan.Wedang kacang memiliki cita rasa manis gurih yang khas.</p>
+                            </div>
+                          </div>
+                          <div class="card">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+      </div>
+      </td>
+      </tr>
+    </div>
+    </table>
+
+
+    <div class="makanan2" style="padding-top:50px;">
+      <table>
+        <tr>
+          <td>
+            <div class="list-product">
+              <div class="container-fluid">
+                <div class="row" style="padding-bottom:-1000px;">
+                  <div class="col">
+                    <div class="card">
+                      <img src="./img/minum5 bandrek cianjur.jpg" alt="Avatar" class="img-fluid">
+                      <div class="overlay">
+                        <div class="text">
+                          <p style="font-size:15px; text-align:center">Bandrek Cianjur</p>
+                          <hr>
+                          <p style="font-size:10px; text-align:center;">Bandrek merupakan minuman hangat yang terbuat dari bahan utamanya jahe dan gula aren yang ditambah dengan serai. Bandrek sangat cocok diminum
+                            pada saat cuaca sedang hujan atau dingin.</p>
+                        </div>
+                      </div>
+                      <div class="card">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    </div>
+    </td>
+
+    <td>
+      <div class="list-product">
+        <div class="container-fluid">
+          <div class="row" style="padding-bottom:-1000px;">
+            <div class="col">
+              <div class="card">
+                <img src="./img/minum6es-goyobod bandung.jpg" alt="Avatar" class="img-fluid">
+                <div class="overlay">
+                  <div class="text">
+                    <p style="font-size:15px; text-align:center">Es Goyobod Bandung</p>
+                    <hr>
+                    <p style="font-size:10px; text-align:center;">Minuman khas Jawa Barat ini Es goyobod yang dingin segar ini punya ciri khas yakni berisi potongan goyobod yakni adonan tepung hunkwe.</p>
+                  </div>
+                </div>
+                <div class="card">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </td>
+
+    <td>
+      <div class="list-product">
+        <div class="container-fluid">
+          <div class="row" style="padding-bottom:-1000px;">
+            <div class="col">
+              <div class="card">
+                <img src="./img/minum7 kopii joss yogya.jpg" alt="Avatar" class="img-fluid">
+                <div class="overlay">
+                  <div class="text">
+                    <p style="font-size:15px; text-align:center">Kopi joss yogyakarta</p>
+                    <hr>
+                    <p style="font-size:10px; text-align:center;">kopi joss tersebut langsung dicampurkan dengan potongan arang yang masih membara sehingga mengeluarkan suara 'josss'</p>
+                  </div>
+                </div>
+                <div class="card">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </td>
+
+    <td>
+      <div class="list-product">
+        <div class="container-fluid">
+          <div class="row" style="padding-bottom:-1000px;">
+            <div class="col">
+              <div class="card">
+                <img src="./img/minum8 es gempol solo.jpg" alt="Avatar" class="img-fluid">
+                <div class="overlay">
+                  <div class="text">
+                    <p style="font-size:15px; text-align:center">Es Gempol solo</p>
+                    <hr>
+                    <p style="font-size:10px; text-align:center;">minuman dari gempol , santan, dan gula cair. Kata gempol berarti adonan dari tepung beras yang dibentuk, lalu diwarnai dan akhirnya dikukus.</p>
+                  </div>
+                </div>
+                <div class="card">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </td>
+
+
+    </tr>
+    </table>
+    </div>
+  </section>
+
+  </section>
+
+  <!-- akhir menu minuman  -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
+
 </html>
