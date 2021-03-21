0d4f0813ea88fd29f833c6d86d995030d98757b0
astutimega <putrimega141@gmail.com>
2021-03-21 19:56:28 +0700

    menambahkan css anggota 1

--- index.php
+++ index.php
@@ -9,6 +9,54 @@
     integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 <body>
+ <!-- navbar -->
+ <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+           <!-- Image and text -->
+                <a class="navbar-brand" href="#">
+                          <img src="img/8nav.png" alt="" width="140" height="40" class="d-inline-block align-top">
+               
+                </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll nav justify-content-end ms-auto"
+                        style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Food</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Drink</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Desserts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Snacks</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+
+                </ul>
+                </li>
+
+                </ul>
+    
+            </div>
+        </div>
+    </nav>
+ 
+
+    <!-- akhir navbar -->
+
+
+
+
+
+
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
