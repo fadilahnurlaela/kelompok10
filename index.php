acb4cde1f084b195caaece8aa0096086f6e2d0fa
astutimega <putrimega141@gmail.com>
2021-03-21 20:04:43 +0700

    menambahkan caption

--- index.php
+++ index.php
@@ -100,6 +100,17 @@
     </div>
     <!-- akhir carousel -->
 
+    <!-- caption -->
+    <section class="caption"style="padding-top:40px;">
+        <div class="col-md-8 mx-auto text-center">
+            <h3>Information About Food By Fameal</h3>
+            <h6>“The chief pleasure in eating does not consist in costly seasoning, or exquisite flavor, but in
+                        yourself.”</h6>
+                    <p>-Horace-</p>
+        </div>
+    </section>
+    <!-- akhir caption -->
+
 
 
 
